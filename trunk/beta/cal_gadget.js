91ae40298a68cddb246c8c0752ad2ec6fbae5167
kpaul86@gmail.com <kpaul86@gmail.com@6cbe1869-4e89-dcf5-f976-1ad0968e04c1>
2013-06-24 14:33:56 +0000

    fixed line breaks

--- trunk/beta/cal_gadget.js
+++ trunk/beta/cal_gadget.js
@@ -8,7 +8,7 @@ var version = '0.8.4b';
 var div = document.createElement("div");
 div.id = 'status';
 div.innerHTML = 'Please upgrade to the latest version of the MyEpisodes gadget!<br><div style="text-align: center;"><a href="https://www.google.com/calendar/render?gadgeturl=https://my-episodes-2-ical.googlecode.com/svn/trunk/myepisodes2ical.xml" target="_top">Upgrade Now!</a></div>';
-alert('Please upgrade to the latest version of the MyEpisodes&nbsp;gadget!/n/nUse the version link at the bottom of the gadget to visit the project homepage and update to the new gadget by clicking the "Add to Google Calendar" button');
+alert('Please upgrade to the latest version of the MyEpisodes&nbsp;gadget!\n\nUse the version link at the bottom of the gadget to visit the project homepage and update to the new gadget by clicking the "Add to Google Calendar" button');
 msg.createStaticMessage(div);
 
 // IE & FF use strict standards for CSS (cross-content) and breaks the gadget styles
